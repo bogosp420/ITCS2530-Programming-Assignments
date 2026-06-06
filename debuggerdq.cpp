745a81c7751fefd72957a1f9dbefb319d4c012d5
bogosp420 <bogosp420@macomb.edu>
2026-06-06 12:36:10 -0400

    Add Week 04 repetition assignment

---
+++ debuggerdq.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Paul Bogos
+
+int main()
+{
+    string name = "Paul";
+    int age = 20;
+    double gpa = 3.7;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "GPA: " << gpa << endl;
+
+    return 0;
+}
